eb99ee3345e419b7d739dd7eebd1775de7bef75a
come97 <comelescure@gmail.com>
2025-10-24 13:26:11 +0200

    fix :add suffix on column name on order

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,7 +1,7 @@
 select
-    order_id_x,
+    order_id AS order_id_x,
     user_name as user_id_x,
-    order_status_x,
+    order_status AS order_status_x,
     DATETIME(order_date, "Europe/Paris") AS order_created_at_x,
     DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_x,
     DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_x,
